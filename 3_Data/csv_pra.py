37348e48724e3b651af72189fd4b0700d84f53dc
ruka_charo <ruka.and.little.charo1@gmail.com>
2021-03-25 21:00:13 +0900

    Study_day16

--- 3_Data/csv_pra.py
+++ 3_Data/csv_pra.py
@@ -1,7 +1,7 @@
-import codecs
+import codecs, csv
 
 
-# Shift_JISのCSVファイルを読む
+#%% Shift_JISのCSVファイルを読む
 filename = 'list-sjis.csv'
 csv = codecs.open(filename, 'r', 'shift_jis').read()
 
@@ -17,3 +17,21 @@ for row in rows:
 # 変換結果を表示
 for c in data:
     print(c[1], c[2])
+
+
+#%% CSVファイルを開く
+filename = 'list-sjis.csv'
+fp = codecs.open(filename, 'r', 'shift_jis')
+
+#1行ずつ読む
+reader = csv.reader(fp, delimiter=',', quotechar='"')
+for cells in reader:
+    print(cells[1], cells[2])
+
+#%% CSVファイルに書き込む
+with codecs.open('test.csv', 'w', 'shift_jis') as fp:
+    writer = csv.writer(fp, delimiter=',', quotechar='"')
+    writer.writerow(['ID', '商品名', '値段'])
+    writer.writerow(['1000', 'SDカード', 300])
+    writer.writerow(["1001", "キーボード", 2100])
+    writer.writerow(["1002", "マジック(赤,青)", 150])
